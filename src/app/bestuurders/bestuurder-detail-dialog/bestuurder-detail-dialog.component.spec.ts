e429cd36a52282175ed746ba597477794892c545
Maneno Baraka <2nis.maneno@gmail.com>
2022-06-03 09:20:30 +0200

    start base for the list and detail screen

---
+++ src/app/bestuurders/bestuurder-detail-dialog/bestuurder-detail-dialog.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BestuurderDetailDialogComponent } from './bestuurder-detail-dialog.component';
+
+describe('BestuurderDetailDialogComponent', () => {
+  let component: BestuurderDetailDialogComponent;
+  let fixture: ComponentFixture<BestuurderDetailDialogComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BestuurderDetailDialogComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BestuurderDetailDialogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
