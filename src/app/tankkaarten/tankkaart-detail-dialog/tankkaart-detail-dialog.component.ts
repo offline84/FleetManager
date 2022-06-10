858f9bc0eef610669fe7d2751c75a7ce5bd3735f
Cédric van Aubel <cedric.vanaubel@gmail.com>
2022-06-10 15:54:44 +0200

    Docuemntation

--- src/app/tankkaarten/tankkaart-detail-dialog/tankkaart-detail-dialog.component.ts
+++ src/app/tankkaarten/tankkaart-detail-dialog/tankkaart-detail-dialog.component.ts
@@ -6,11 +6,10 @@ import {Tankkaart} from "../../objects/tankkaart";
 import {ITankkaart} from "../../objects/iTankkaart";
 import {DataExchangeService} from '../../data-exchange.service';
 import {MatBottomSheet, MatBottomSheetConfig} from '@angular/material/bottom-sheet';
-import {
-  TankkaartDeleteConfirmationSheetComponent
-} from "../tankkaart-delete-confirmation-sheet/tankkaart-delete-confirmation-sheet.component";
+import {TankkaartDeleteConfirmationSheetComponent} from "../tankkaart-delete-confirmation-sheet/tankkaart-delete-confirmation-sheet.component";
 import {Brandstof} from "../../objects/brandstof";
 import {mogelijkeBrandstof} from "../../objects/mogelijkeBrandstof";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-tankkaart-detail-dialog',
@@ -20,28 +19,101 @@ import {mogelijkeBrandstof} from "../../objects/mogelijkeBrandstof";
 
 export class TankkaartDetailDialogComponent implements OnInit {
 
-  // class in htmlcode voor het omschrijven van gebeurtenissen
+  /**
+   * ngClass
+   *
+   * HTML Class voor het omschrijven van gebeurtenissen gericht naar de gebruiker.
+   *
+   * @return {string} message (nullable)   dynamisch gegenereerd bericht.
+   */
   @ViewChild('message', { static: false}) message!: ElementRef;
 
+  /**
+   * ngModel
+   *
+   * modifiable behandelt de modus waarin de input van een tankkaart kan aangepast worden.
+   */
   modifiable: boolean = true;
+
+  /**
+   * ngModel
+   *
+   * forCreation behandelt de modus waarin de input van een tankkaart kan aangemaakt worden.
+   */
   forCreation: boolean = true;
+
+  /**
+   * ngclass
+   *
+   * notEditable verandert de kleur van niet aanpasbare input naar CSS class changeColor
+   *
+   * @example
+   * forCreation = false
+   * modifiable = true
+   * notEditable = "changeColor"       => kleur van input = grijs
+   */
   notEditable!: string;
+
+  /**
+   * ngClass
+   *
+   * viewOnly verandert de kleur van niet aanpasbare input naar CSS class changeColor
+   *
+   * @example
+   * modifiable = false
+   * forCreation = false
+   * viewOnly = "changeColor"           => kleur van input = grijs
+   */
   viewOnly!: string;
+
+  /**
+   * ngClass
+   *
+   * hide verbergt de pincode
+   */
   hide: boolean = true;
+
+  /**
+   * indien een tankkaart wordt meegegeven door de parent (TankkaartListComponent) of door de DataExchangeService
+   * wordt deze in deze variabele gegoten.
+   *
+   * [DataExchangeService]{@link DataExchangeService}
+   */
   tankkaart = new Tankkaart();
 
+  /**
+   * Omvat alle geseede brandstoftypes uit de database.
+   * wordt opgevuld tijdens OnInit.
+   */
   KeuzeBrandstoffen: any;
+
+  /**
+   * Omvat de lijst van alle Bestuurders die niet gelinkt zijn aan een tankkaart uit de database.
+   * wordt opgevuld tijdens OnInit.
+   */
   unlinkedBestuurders: any;
+
+  /**
+   * Bevat de bestuurder die gelinkt is aan de property tankkaart.
+   * wordt geïnitialiseerd tijdens OnInit.
+   */
   bestuurderLink: any = null;
 
+  /**
+   * Template voor brandstof
+   */
   brandstof={
     id: "",
     typeBrandstof: ""
   };
 
 
-  // deze Formgroep behandelt de validatie en controls van de inputs en selects. Bij objecten is het raadzaam deze te flattenen of enkel
-  // de benodigde properties weer te geven. Later worden deze terug omgezet in objecten. zie function: CreateObjectToSend
+  /**
+   * Behandelt de validatie en controls van de inputs en selects. Bij objecten is het raadzaam deze te flattenen of enkel
+   * de benodigde properties weer te geven. Later worden deze terug omgezet in objecten.
+   *
+   * [CreateObjectToSend] {@link CreateObjectToSend()}
+   */
   tankkaartForm = new FormGroup({
     kaartnummer: new FormControl('',[Validators.required]),
     geldigheidsdatum: new FormControl(new Date().toISOString().slice(0,-5),[Validators.required]),
@@ -50,10 +122,19 @@ export class TankkaartDetailDialogComponent implements OnInit {
     typeBrandstof: new FormControl('',),
   });
 
+  /**
+   * @param datastream Httpclient naar de back- end toe.
+   * @param dialogRef referentie naar het dialog venster zelf.
+   * @param dataService Service voor het behandelen van data tussen componenten zonder parent - child relatie.
+   * @param bottomSheet instantie nodig voor de initialisatie van het BottomsheetComponent.
+   * @param router instantie die de routes van de applicatie verzorgt.
+   * @param data {any} de data die wordt meegegeven bij het openen van het dialogvenster.
+   */
   constructor(private datastream: DatastreamService,
               private dialogRef: MatDialogRef<TankkaartDetailDialogComponent>,
               private dataService: DataExchangeService,
               private bottomSheet: MatBottomSheet,
+              private router: Router,
               @Inject(MAT_DIALOG_DATA) private data: any) {
     this.tankkaart = data.entity;
     this.modifiable = data.modifiable;
@@ -67,7 +148,7 @@ export class TankkaartDetailDialogComponent implements OnInit {
       this.patchObjectToForm(this.tankkaart);
     }
 
-    // zet de mode waarin de dialog zich op dit moment bevindt. de mogelijkheden zijn: boolean modifialbe.
+    // zet de mode waarin de dialog zich op dit moment bevindt.
     //Deze wordt meegegeven in de MAT_DIALOG_DATA bij opening van de dialog.
     this.IsModifiable(this.modifiable);
 
@@ -101,7 +182,9 @@ export class TankkaartDetailDialogComponent implements OnInit {
     });
   }
 
-  //Omvat de creatie van het te verzenden object en de wissel van mode "add" naar "view" + errorbehandeling.
+  /**
+   * Omvat de creatie van het te verzenden object en de wissel van mode "add" naar "view" + errorbehandeling.
+   */
   onSubmit = () => {
 
     let fuelcard = this.CreateObjectToSend();
@@ -119,6 +202,9 @@ export class TankkaartDetailDialogComponent implements OnInit {
     );
   }
 
+  /**
+   * Past de modus voor modificatie van detailweergave naar editeren aan.
+   */
   openUpdateScreen = () => {
     this.forCreation = false;
     this.IsModifiable(true);
@@ -126,6 +212,11 @@ export class TankkaartDetailDialogComponent implements OnInit {
     this.viewOnly = "";
   }
 
+  /**
+   * Opent de geïnjecteerde instantie van de MatBottomSheetModule voor verwijdering van het voertuig en definieert de te verwijderen entiteit.
+   *
+   * [DeleteConfirmationSheetComponent] {@link DeleteConfirmationSheetComponent}
+   */
   openDeleteScreen = () => {
     const config = new MatBottomSheetConfig();
     config.autoFocus = true;
@@ -144,22 +235,43 @@ export class TankkaartDetailDialogComponent implements OnInit {
     });
   }
 
-  //Aangezien mat-select werkt met een formcontrol en deze hier niet is aangemaakt omdat de bestuurderLink een object omvat, implementeren we de selectie handmatig via een eventlistener.
+  /**
+   * Aangezien mat-select werkt met een formcontrol en deze hier niet is aangemaakt omdat de bestuurderLink een object omvat,
+   *  implementeren we de selectie handmatig via een eventlistener.
+   *
+   * @param event
+   *
+   * @example
+   *
+   * <div class="formbuttons koppel field" *ngIf="viewOnly">
+   <mat-form-field *ngIf="!voertuig.koppeling" class="link-bestuurder" appearance="fill">
+   <mat-select (selectionChange)="onSelectionChange($event.value)">
+   */
   onSelectionChange = (event: any) => {
     let link = this.unlinkedBestuurders.filter((u: any) => u.rijksregisternummer == event);
     this.bestuurderLink = link[0];
   }
 
-  //To Do route naar bestuurders en open daar automatisch met het behavioursubject de detail dialog voor de meegegeven bestuurder.
-  OpenBestuurdersDetails = () => {
+  /**
+   * Deze method opent bij het klikken op de gelinkte bestuurder in de detail-dialog view mode, de view mode in de bestuurdersectie van de applicatie van desbetreffende bestuurder.
+   * Eerst zorgen we ervoor dat de huidige dialog gesloten wordt, dan geven we via de data exchange service de opdracht om de view dialog te openen
+   * en vervolgens navigeren we naar de bestuurderssectie van de applicatie.
+   *
+   * {@link DataExchangeService}
+   */
+  OpenBestuurdersDetails(): void {
+    this.dialogRef.close();
     this.dataService.sendData("bestuurder","view", this.bestuurderLink);
+    let navi =this.router.navigate(['/bestuurders']);
   }
 
+  /**
+   * Slaat de veranderingen aan de entiteit op en patched deze veranderingen op de entiteit.
+   */
   onSave = () => {
     let fuelcard = this.CreateObjectToSend();
 
     this.datastream.UpdateFuelCard(fuelcard).subscribe( (res: any) =>{
-
         if(res){
           this.tankkaart = res;
           this.patchObjectToForm(res);
@@ -173,10 +285,13 @@ export class TankkaartDetailDialogComponent implements OnInit {
     );
   }
 
-  // Omdat we enkel het correcte resultaat willen weergeven en deze in de tabel updaten voor geslaagde patch-bewerkingen naar de API,
-  //gebruiken we de depricated manier van httpclient. + errormessagebehandeling.
+  /**
+   * Koppelt of ontkoppelt en bestuurder aan de entiteit.
+   *
+   * Omdat we enkel het correcte resultaat willen weergeven en deze in de tabel updaten voor geslaagde patch-bewerkingen naar de API,
+   * gebruiken we de depricated manier van httpclient. + errormessagebehandeling.
+   */
   linkUnlinkDriver = () =>{
-
     if(this.bestuurderLink){
       if(this.tankkaart.koppeling){
         this.datastream.UnlinkFuelCard(this.tankkaart.kaartnummer).subscribe(() =>{
@@ -213,7 +328,11 @@ export class TankkaartDetailDialogComponent implements OnInit {
     }
   }
 
-  //behandelt de mode waarin de dialog zich bevindt.
+  /**
+   * behandelt de modus waarin de DialogComponent zich op dit moment bevindt.
+   *
+   * @param ismodifiable {boolean} boolean voor de check of de entiteit bewerkbaar is op dat moment.
+   */
   IsModifiable = (ismodifiable: boolean) =>{
     this.modifiable = ismodifiable
     if(!ismodifiable){
@@ -223,8 +342,14 @@ export class TankkaartDetailDialogComponent implements OnInit {
     }
   }
 
-  //indien een tankkaart is meegegeven wordt deze via deze method gepatched met de tankkaartForm.
-  //De niet gepatchede controls worden handmatig ingegeven.
+  /**
+   * indien een entiteit is meegegeven wordt deze via deze method gepatched met de Form(s).
+   * De niet automatisch gepatchede controls worden handmatig ingegeven.
+   *
+   * Manueel: geldigheidsDatum en mogelijkeBrandstoffen
+   *
+   * @param entity de entiteit die in de form dient gegoten te worden.
+   */
   patchObjectToForm = (entity: Tankkaart) =>{
     this.tankkaartForm.patchValue(this.tankkaart);
     this.tankkaartForm.controls["geldigheidsdatum"].setValue(entity.geldigheidsDatum.toString());
@@ -239,14 +364,22 @@ export class TankkaartDetailDialogComponent implements OnInit {
     }
   }
 
+  /**
+   * Bereidt de entiteit voor voor verzending naar de back- end.
+   * Elke property dient meegegeven te worden aan de api, null waardes voor getallen en strings kunnen niet verwerkt worden
+   * en resulteert tot een error van de API.
+   *
+   * Pincode: default 9999
+   * MogelijkeBrandstoffen: opbouw van de brandstoffen lijst in de mogelijke brandstoffen
+   *
+   * @returns new Entity();
+   */
   CreateObjectToSend =(): ITankkaart => {
     let fuelcard = new Tankkaart();
 
     fuelcard.kaartnummer = this.tankkaartForm.controls["kaartnummer"].value;
     fuelcard.geldigheidsDatum = this.tankkaartForm.controls["geldigheidsdatum"].value;
     fuelcard.isGeblokkeerd = this.tankkaartForm.controls["isGeblokkeerd"].value;
-
-    // Elke property dient meegegeven te worden aan de api, null waardes voor getallen en strings kunnen niet verwerkt worden.
 
     if(!this.tankkaartForm.controls["pincode"].value){
       this.tankkaartForm.controls["pincode"].setValue(9999);
